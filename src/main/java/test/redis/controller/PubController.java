72b4b20d216579964611c14277ae511e93b8e799
Sijun <sionwer4@naver.com>
2023-07-26 17:46:28 +0900

    feat: Create pub controller to test sending message

---
+++ src/main/java/test/redis/controller/PubController.java
@@ -0,0 +1,22 @@
+package test.redis.controller;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import test.redis.entity.ChatMessage;
+import test.redis.service.PubService;
+
+@RestController
+public class PubController {
+
+    private final PubService redisPubService;
+
+    public PubController(PubService redisPubService) {
+        this.redisPubService = redisPubService;
+    }
+
+    @PostMapping("/chat")
+    public void sendMessage(@RequestBody ChatMessage chatMessage) {
+        redisPubService.sendMessage(chatMessage);
+    }
+}
