ff355f3b4b637d1aba767b276548b83e8667a543
Sijun <sionwer4@naver.com>
2023-07-26 20:39:37 +0900

    fix: Eliminate configuration annotation from local redis config

--- src/main/java/test/redis/configuration/RedisLocalConfiguration.java
+++ src/main/java/test/redis/configuration/RedisLocalConfiguration.java
@@ -11,7 +11,6 @@ import org.springframework.data.redis.listener.adapter.MessageListenerAdapter;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 import test.redis.service.SubService;
 
-@Configuration
 public class RedisLocalConfiguration {
 
 
