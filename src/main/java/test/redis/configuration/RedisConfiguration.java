ab38683e1718d1b24e961c38210f8b6c721930c7
Sijun <sionwer4@naver.com>
2023-07-26 17:41:27 +0900

    feat: Add listener setting to redis configuration

--- src/main/java/test/redis/configuration/RedisConfiguration.java
+++ src/main/java/test/redis/configuration/RedisConfiguration.java
@@ -6,9 +6,13 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.listener.ChannelTopic;
+import org.springframework.data.redis.listener.RedisMessageListenerContainer;
+import org.springframework.data.redis.listener.adapter.MessageListenerAdapter;
 import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 import test.redis.entity.ChatMessage;
+import test.redis.service.SubService;
 
 @Configuration
 public class RedisConfiguration {
@@ -30,5 +34,18 @@ public class RedisConfiguration {
         redisTemplate.setKeySerializer(new StringRedisSerializer());
         redisTemplate.setValueSerializer(new Jackson2JsonRedisSerializer<>(ChatMessage.class));
         return redisTemplate;
+    }
+
+    @Bean
+    MessageListenerAdapter messageListenerAdapter() {
+        return new MessageListenerAdapter(new SubService());
+    }
+
+    @Bean
+    RedisMessageListenerContainer redisContainer() {
+        RedisMessageListenerContainer container = new RedisMessageListenerContainer();
+        container.setConnectionFactory(redisConnectionFactory());
+        container.addMessageListener(messageListenerAdapter(), new ChannelTopic("topic1"));
+        return container;
     }
 }
